83721ea474cfc9e80e7f82ccff1411b23b780c70
klaudiakn <selecionele@gmail.com>
2017-01-24 17:56:11 +0100

    Delete CScreen.java

--- dwgame/CScreen.java
+++
@@ -1,6 +0,0 @@
-package dwgame;
-
-//Interfejs ustawiający, że wszystkie ekrany pamiętają swojego rodzica
-public interface CScreen {
-    public void setScreenParent(dwgame.Rooms.RoomController screenPage);
-}
